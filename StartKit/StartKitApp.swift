21118edb00a4fbb73bd690adf20b56e831c91503
vfa-minhsn <minhsn@vitalify.asia>
2026-05-14 09:48:07 +0700

    Initial Commit

---
+++ StartKit/StartKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartKitApp.swift
+//  StartKit
+//
+//  Created by vfa on 14/5/26.
+//
+
+import SwiftUI
+
+@main
+struct StartKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StartKitTests/StartKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  StartKitTests.swift
+//  StartKitTests
+//
+//  Created by vfa on 14/5/26.
+//
+
+import Testing
+@testable import StartKit
+
+struct StartKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
